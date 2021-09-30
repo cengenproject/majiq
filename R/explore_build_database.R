8e194b836c2d5949146a3185100f55ad97ed34be
AlexWeinreb <alexis.weinreb@yale.edu>
2021-09-30 13:19:59 -0400

    Save script to open the database and peek into it.

---
+++ R/explore_build_database.R
@@ -0,0 +1,45 @@
+# Check build DB
+library(DBI)
+
+con <- dbConnect(RSQLite::SQLite(), "data/splicegraph.sql")
+
+
+dbListTables(con)
+
+# Junction ----
+dbListFields(con, "junction")
+
+res <- dbSendQuery(con, "SELECT gene_id FROM junction
+                              WHERE has_reads AND NOT is_constitutive;")
+xx <- dbFetch(res)
+dbClearResult(res)
+
+
+head(xx)
+nrow(xx)
+
+length(unique(xx$gene_id))
+
+
+
+# Junction reads ----
+dbListFields(con, "junction_reads")
+
+
+res <- dbSendQuery(con, "SELECT junction_gene_id FROM junction_reads
+                              WHERE reads > 3;")
+xx <- dbFetch(res)
+dbClearResult(res)
+
+
+head(xx)
+nrow(xx)
+
+length(unique(xx$junction_gene_id))
+
+
+
+
+
+
+# dbDisconnect(con)
