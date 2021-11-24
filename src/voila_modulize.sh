75640c910d564d54df8a031289ea9e2605762b34
AlexWeinreb <alexis.weinreb@yale.edu>
2021-11-24 14:59:14 -0500

    bug: load VOILA to modulize.

--- src/voila_modulize.sh
+++ src/voila_modulize.sh
@@ -7,6 +7,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alexis.weinreb@yale.edu
 
+
+source /home/aw853/bin/majiq_v2-3/env/bin/activate
+module load Python/3.8.6-GCCcore-10.2.0 HTSlib/1.12-GCCcore-10.2.0
+
+
+
+
 outdir='/gpfs/ycga/project/ysm/hammarlund/aw853/counts/majiq/2021-11-10_outs'
 
 mkdir -p $outdir/voila_modulizer
